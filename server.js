8c639181bf487b78ddf035f2a72bc7d0121fa3ed
Harsh <harshtanishq2002@gmail.com>
2024-06-26 15:49:33 +0530

    feat : connected

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+//get a list of ifve jokes
+
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      setup: "Why did the scarecrow win an award?",
+      punchline: "Because he was outstanding in his field!",
+    },
+    {
+      id: 2,
+      setup: "What do you call a bear with no teeth?",
+      punchline: "A gummy bear!",
+    },
+    {
+      id: 3,
+      setup: "Why did the bicycle fall over?",
+      punchline: "Because it was two-tired!",
+    },
+  ];
+  res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
